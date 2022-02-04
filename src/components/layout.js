2bbf2dcb0d3093dcbfb7aaeaadae21185365e72e
Ashish Kanwar Singh <ashishkanwarsingh@gmail.com>
2022-02-04 17:42:22 -0500

    footer color changed

--- src/components/layout.js
+++ src/components/layout.js
@@ -44,7 +44,7 @@ const Layout = ({ children }) => {
           }}
         >
           <SocialMedia/>
-          © {new Date().getFullYear()}, All rights reserved.
+          <span className="has-text-grey">© {new Date().getFullYear()}, All rights reserved.</span>
         </footer>
         <a className='button is-success static-button is-hidden-mobile' target='_blank' rel="noopener noreferrer" href='https://www.eventbrite.ca/e/senecas-sustainability-hackathon-2022-registration-205162405277'>
             <span>Register Now!</span>
