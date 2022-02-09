f5012fa77c21ace5c37d599cb21c6c81b1f80f2c
Ashish Kanwar Singh <ashishkanwarsingh@gmail.com>
2022-02-09 14:13:39 -0500

    winners section updated

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
                 <a className='button is-success static-button is-hidden-mobile' target='_blank' rel="noopener noreferrer" href='https://www.eventbrite.ca/e/senecas-sustainability-hackathon-2022-registration-205162405277'>
                     <span>Register Now!</span>
                 </a>
-                <SocialMedia classes="floating-left"/>
+                <SocialMedia classes="floating-left is-hidden-mobile"/>
             </div>
         </>
     )
