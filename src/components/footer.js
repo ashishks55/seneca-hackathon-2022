f621637d2cafdf05b9178f906fe3048bd4533f85
Ashish Kanwar Singh <ashishkanwarsingh@gmail.com>
2022-02-09 14:33:33 -0500

    footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PastEventsBackgroundImage from '../images/footer/past_events_background_image.png';
 import Hackathon2021Logo from '../images/footer/hackathon_2021_logo.png';
+import SocialMedia from './reusable/socialmedia';
 
 const Footer = () => {
     return (
@@ -9,7 +10,7 @@ const Footer = () => {
             borderTop: '4px solid #1C444C',
             paddingTop: '3rem'
         }}>
-            <div style={{ width: '100%', marginBottom: '5%' }}>
+            {/* <div style={{ width: '100%', marginBottom: '5%' }}>
                 <div className="has-text-centered" style={{
                     width: '50%', display: 'inline-block', verticalAlign: 'top'
                 }}>
@@ -26,16 +27,16 @@ const Footer = () => {
                         backgroundRepeat: 'no-repeat'
                     }}><a href="http://digitalhealth.senecahackathon.com/"><img src={Hackathon2021Logo} style={{ backgroundColor: 'white', width: '20%', height: '20%', verticalAlign: 'right', marginLeft: '80%' }}></img></a></div>
                 </div>
-            </div>
+            </div> */}
+            <SocialMedia classes={'is-hidden-tablet'}/>
             <div style={{
                 marginBottom: '0rem',
-                border: '10px solid rgb(28, 68, 76)',
                 width: '100%',
                 height: '3rem',
-                backgroundColor: 'rgb(28, 68, 76)',
-                color: 'white'
+                backgroundColor: 'transparent',
+                color: '#1C444C'
             }}>
-                <span>Copyright © {new Date().getFullYear()} Seneca Hackathon 2022. All rights reserved.</span>
+                <p className='has-text-centered'>Copyright © {new Date().getFullYear()} Seneca Hackathon 2022. All rights reserved.</p>
             </div>
         </footer>
     );
