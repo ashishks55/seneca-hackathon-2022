e8ee1e3ceb135912c592eb6fcf1ce241f247a905
rugma21 <rmuraleedharan-sandh@myseneca.ca>
2022-02-16 15:16:21 -0500

    modifications in faq

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -2,47 +2,90 @@ import React from 'react';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Accordion from '../components/reusable/accordion';
+import "./faq.css"
+
+
 
 const IndexPage = () => {
     const faqs = [
         {
-            qn: 'How to Register',
-            ans: `All you need to do is click on the <a target='_blank' rel="noopener noreferrer" href='https://www.eventbrite.ca/e/senecas-sustainability-hackathon-2022-registration-205162405277' class='has-text-bold is-underlined has-text-green'>registration link</a> and register using our eventbrite page.`
+            qn: 'Is the hackathon only for programmers?',
+            ans: `Absolutely not! The hackathon is open to all disciplines. If you are passionate about sustainable causes, you can participate in this hackathon! `
         },
         {
-            qn: 'Who can attend?',
-            ans: `Registration are open to Seneca students. For additional colleges and universities open from <b>Jan 21, 2022</b>. Participating colleges and universities will be listed. If you are NOT a student in any of the qualifying institutions, you may still participate, but MUST be assigned to a team anchored by Seneca Students. The Student Success Team will help you accomplish this.`
+            qn: 'How can I participate? What will I do?',
+            ans: `You must have registered through Eventbrite by now. Through the next two weeks, we will gradually release information on challenge sets. If you see a solution to a challenge set, update your registration on Eventbrite and lock in your position as a contender. Over the hackathon week, you will work with your team to come up with a solution to your chosen challenge to be presented in video format.`
         },
         {
-            qn: 'First Time Hackers?',
-            ans: `No worries, We will help start your hackathon journey with awesome workshops and events which will help strengthen your problem solving skills.`
+            qn: 'What are the challenge sets? How many are there? ',
+            ans: `We are expecting 12-15 challenge sets. The latest challenge set information is available in the challenge set section of this website! `
         },
         {
-            qn: 'Team Formation?',
-            ans: `You can form team of a maximum of <b>5 members</b>. If you don't have a team, you can meet other peer hackers from across the globe at the event and form a team!`
+            qn: 'Who are the Student Success Officers (SSO)? ',
+            ans: `Once you have chosen your challenge set, you will be assigned an SSO. The SSO is your go-to person for any questions you have about your hackathon experience. They will be in contact with you throughout the hackathon to help you make a successful submission. `
+        },
+        {
+            qn: 'What are the prizes? ',
+            ans: `Winners will receive some cash, exclusive hackathon merch, and hackathon winner micro-credential. Some challenge sets have additional prizes so make sure to check them all out!`
         }
     ]
+    const faqsright = [
+        {
+            qn: 'Are there team requirements? ',
+            ans: `You are expected to participate with at least one other person. However, the maximum team size is 5. Participants from any school are welcome, but if your school is not listed as one of our hackathon partner schools, you will need to work with at least one Seneca student. Partner schools: PIM, U del Rosario, St. Clair College, Conestoga College. `
+        },
+        {
+            qn: 'What is the event schedule?',
+            ans: `The event is scheduled to run during reading week: From Feb 28 to Mar 4. An official event schedule will be available soon.`
+        },
+        {
+            qn: 'What are the requirements to get a micro-credential?',
+            ans: `You will be awarded with one of two micro-credentials: Participant or Winner. Winner micro-credentials are reserved for challenge set winners, category winners, and the best-in-show. Participant micro-credentials will be awarded to teams that document their daily progress and make a submission. More detailed micro-credential information will be available soon. `
+        },
+        {
+            qn: 'Will I get free stuff?  ',
+            ans: `Our social media is the best place to win free stuff. Check out our social media for the latest hackathon giveaways! `
+        } 
+    ]
     return (
-  <Layout>
-    <Seo title="FAQ" />
-    <section className="hero is-medium is-white">
-        <div className="hero-body hero-body">
-            <div className='columns'>
-                <div className='column is-10 is-offset-1'>
-                    <h1 className="title">FAQ</h1>
-                    <div className='accordion-list'>
-                        {
-                            faqs.map((faq, index) => (
-                                <Accordion title={faq.qn} info={faq.ans} key={index}/>
-                            ))
-                        }
+        <Layout>
+            <Seo title="FAQ" />
+            <section className="hero is-medium is-white">
+                <div className="hero-body hero-body">
+                    <h1 className="faq-title">Frequently Asked Questions</h1>
+                    {/* <div className='columns'>
+                        <div className='column is-10 is-offset-1'>
+                            <h1 className="title">Frequently Asked Questions</h1>
+                            <div className='accordion-list'>
+                                {
+                                    faqs.map((faq, index) => (
+                                        <Accordion title={faq.qn} info={faq.ans} key={index}/>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div> */}
+                    
+                    <div class="flex-container">
+                        <div class="flex-item-left">
+                            {
+                                faqs.map((faq, index) => (
+                                    <div className='faq-list'><Accordion title={faq.qn} info={faq.ans} key={index}/></div>
+                                ))
+                            }
+                        </div>
+                        <div class="flex-item-right">  
+                            {
+                                faqsright.map((faql, index) => (
+                                    <div className='faq-list'><Accordion title={faql.qn} info={faql.ans} key={index}/></div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-  </Layout>
-);
+            </section>
+        </Layout>
+    );
 };
 
 export default IndexPage
