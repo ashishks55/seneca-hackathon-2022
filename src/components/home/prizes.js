1247d5b69ea64cb93516583324a7fb2029fcea9a
nisha-1991 <npmagnani@myseneca.ca>
2022-02-15 17:58:12 -0700

    Made changes to align it with other sections

--- src/components/home/prizes.js
+++ src/components/home/prizes.js
@@ -10,7 +10,7 @@ const prizes = () => {
             <div className="hero-body hero-body-3">
                 <div className='columns'>
                     <div className='column is-12-tablet is-5-desktop is-offset-1 hero-content'>
-                        <div className='is-hidden-desktop'><br /><br /></div>
+                        <h1 className="title">Prizes</h1><br />
                         <p className="prize-title has-text-dark-green">
                             <span>GRAND PRIZE</span>
                         </p>
@@ -22,16 +22,18 @@ const prizes = () => {
                             <br />
                             <span className='has-text-dark-green'><b>a chance to present your idea to the investor</b></span>
                         </p>
-                        <p className="subtitle is-size-6 mg-top-1 has-text-orange">
+                        <p className="subtitle is-size-6 mg-top-1 has-text-orange" style={{ marginRight: '8.333%' }}>
                             <b>All challenge set winners get WINNER Micro credential & sustainable Hacktahon Tees</b>
                         </p>
-                        <img src={grand_prize} alt="Grand Prize" width="100%" className='is-hidden-tablet' />
+                        <div className='column is-5 is-hidden-desktop hero-img'>
+                            <img src={grand_prize} alt="Grand Prize" width="100%" />
+                            </div>
                     </div>
                     <div className='column is-5 is-hidden-touch hero-img'>
                         <img src={grand_prize} alt="Grand Prize" width="100%" />
                     </div>
                 </div>
-                <table border="0" cellspacing="0" cellpadding="0" className="fullWidth">
+                <table border="0" cellspacing="0" cellpadding="0" className="f1-car-table">
                     <tr className='f1-car-tr'>
                         <td className='f1-car-td-white'></td>
                         <td className='f1-car-td-red'></td>
@@ -39,7 +41,7 @@ const prizes = () => {
                         <td className='f1-car-td-red'></td>
                     </tr>
                 </table>
-                <div className='center-text f1-div'>                    
+                <div className='f1-div column is-10 is-offset-1'>
                     <div className='f1-car hero-img'>
                         <img src={f1_car} alt="F1 car" width="100%"/>
                     </div>
@@ -50,15 +52,15 @@ const prizes = () => {
                             <br />
                             31 MONTREAL GP
                         </p>
-                        <p className="title has-text-red">
+                        <p className="prize-title has-text-red">
                             <b>JUNE 17-19</b>
                         </p>
-                        <p className="title mg-top-3">
+                        <p className="prize-title mg-top-3">
                             <b>Exclusively for F1 Challenge set winning team</b>
                             </p>
                     </div>                    
                 </div>
-                <table border="0" cellspacing="0" cellpadding="0" style={{ marginTop: '3.45rem', marginBottom: '3.45rem' }} className="fullWidth">
+                <table border="0" cellspacing="0" cellpadding="0" style={{ marginTop: '3.45rem', marginBottom: '3.45rem' }} className='f1-car-table'>
                     <tr className='f1-car-tr'>
                         <td className='f1-car-td-white'></td>
                         <td className='f1-car-td-red'></td>
@@ -90,8 +92,8 @@ const prizes = () => {
                     <br/>
                     <div className='row prizes-trophy-40-right-div'>
                         <div className='is-hidden-desktop'><br /><br /></div>
-                        <p className='center-text width-60 is-hidden-touch' style={{ backgroundImage: 'url(' + confetti + ')' }}><img className="diagonal-image" src={trophy} alt="Trophy" width="50%" /></p>
-                        <p className='center-text width-60 is-hidden-desktop' style={{ backgroundImage: 'url(' + confetti + ')' }}><img className="diagonal-image" src={trophy} alt="Trophy" width="100%" /></p>
+                        <p className='center-text width-60 is-hidden-touch is-hidden-tablet' style={{ backgroundImage: 'url(' + confetti + ')' }}><img className="diagonal-image" src={trophy} alt="Trophy" width="50%" /></p>
+                        <p className='center-text width-60 is-hidden-desktop' style={{ backgroundImage: 'url(' + confetti + ')' }}><img className="diagonal-image" src={trophy} alt="Trophy" width="80%" /></p>
                     </div>
                     <br/>
                 </div>                
