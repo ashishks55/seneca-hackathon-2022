50201d8ece6edc45b30761f1f81f740a8e3c47db
nisha-1991 <npmagnani@myseneca.ca>
2022-02-18 15:41:13 -0700

    Resolved issues, Made changes to timer and Challenge Set

--- src/components/home/prizes.js
+++ src/components/home/prizes.js
@@ -3,7 +3,6 @@ import grand_prize from '../../images/prizes/Grand prize.png'
 import f1_car from '../../images/prizes/F1 Car.png'
 import trophy from '../../images/prizes/Trophy.png'
 import confetti from '../../images/prizes/Confetti.png'
-import trophySection from '../../images/prizes/TrophySection.png'
 
 const prizes = () => {
     return (
@@ -12,7 +11,7 @@ const prizes = () => {
                 <div className='columns'>
                     <div className='column is-12-tablet is-5-desktop is-offset-1 hero-content no-padding'>
                         <p className="prize-title main-title has-text-dark-green">
-                            <br/><br/><span>GRAND PRIZE</span>
+                            <br /><br /><span>GRAND PRIZE</span>
                         </p>
                         <p className="prize-title has-text-orange grand-prize-title">
                             $2000
@@ -25,9 +24,9 @@ const prizes = () => {
                         <p className="subtitle is-size-6 mg-top-1 has-text-orange" style={{ marginRight: '8.333%' }}>
                             <b>All challenge set winners get WINNER Micro credential & sustainable Hacktahon Tees</b>
                         </p>
-                        <div className='column is-5 is-hidden-desktop hero-img' style={{paddingTop:'6%'}}>
+                        <div className='column is-5 is-hidden-desktop hero-img' style={{ paddingTop: '6%' }}>
                             <img src={grand_prize} alt="Grand Prize" width="70%" />
-                            </div>
+                        </div>
                     </div>
                     <div className='column is-5 is-hidden-touch hero-img' style={{ paddingTop: '6%' }}>
                         <img src={grand_prize} alt="Grand Prize" width="80%" />
@@ -43,11 +42,11 @@ const prizes = () => {
                 </table>
                 <div className='f1-div column is-10 is-offset-1'>
                     <div className='f1-car hero-img'>
-                        <img src={f1_car} alt="F1 car" width="100%"/>
+                        <img src={f1_car} alt="F1 car" width="100%" />
                     </div>
-                    <div className='is-12-tablet is-7-desktop is-offset-1 hero-content f1-content'>                                              
-                            <p className="prize-title">
-                             3 DAY PASSES FOR GRANDSTAND
+                    <div className='is-12-tablet is-7-desktop is-offset-1 hero-content f1-content'>
+                        <p className="prize-title">
+                            3 DAY PASSES FOR GRANDSTAND
                             <br />
                             31 MONTREAL GP
                         </p>
@@ -56,8 +55,8 @@ const prizes = () => {
                         </p>
                         <p className="prize-title mg-top-3">
                             <b>Exclusively for F1 Challenge set winning team</b>
-                            </p>
-                    </div>                    
+                        </p>
+                    </div>
                 </div>
                 <table border="0" cellspacing="0" cellpadding="0" className='f1-car-table-bottom'>
                     <tr className='f1-car-tr'>
@@ -73,25 +72,25 @@ const prizes = () => {
                             <span>Category winners get $500 & an opportunity to present the idea to Thinking North Investor Network</span>
                         </p>
                     </div>
-                    <br/>
+                    <br />
                     <div className='row prizes-trophy-right-div'>
                         <p className='right-text width-60 prize-trophy-section'>
                             Extra $500 for challenge sets from Deloitte, Roche, CPOS and FutureTel
                         </p>
                     </div>
-                    <br/>
+                    <br />
                     <div className='row prizes-trophy-left-div'>
                         <p className='width-60 prize-trophy-section'>
                             <b>All rule abiding participants have chances to win over $1000 worth of prizes in social media - based content & receive a participant micro credential </b>
                         </p>
                     </div>
-                    <br/>
+                    <br />
                     <div className='row prizes-trophy-40-right-div'>
                         <p className='center-text width-60 is-hidden-touch' style={{ backgroundImage: 'url(' + confetti + ')' }}><img className="diagonal-image" src={trophy} alt="Trophy" width="40%" /></p>
                         <p className='center-text width-60 is-hidden-desktop' style={{ backgroundImage: 'url(' + confetti + ')' }}><img className="diagonal-image" src={trophy} alt="Trophy" width="70%" /></p>
                     </div>
-                    <br/>
-                        </div>
+                    <br />
+                </div>
             </div>
         </section>
     );
