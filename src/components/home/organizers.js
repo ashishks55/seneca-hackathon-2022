537d1ee6933b39473a248ef120282aa0bc309847
Ashish Kanwar Singh <ashishkanwarsingh@gmail.com>
2022-02-26 12:12:18 -0500

    layout done

--- src/components/home/organizers.js
+++ src/components/home/organizers.js
@@ -17,6 +17,29 @@ import azher from '../../images/people/azher.jpeg';
 import linkedin_icon from '../../images/linkedin.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import cm_1 from '../../images/logos/cm-1.jpg'
+import cm_2 from '../../images/logos/cm-2.png'
+import cm_3 from '../../images/logos/cm-3.png'
+import cm_4 from '../../images/logos/cm-4.png'
+
+import csr_1 from '../../images/logos/csr-1.png'
+import csr_2 from '../../images/logos/csr-2.png'
+import csr_3 from '../../images/logos/csr-3.png'
+import csr_4 from '../../images/logos/csr-4.png'
+import csr_5 from '../../images/logos/csr-5.jpg'
+
+import gco_1 from '../../images/logos/gco-1.png'
+import gco_2 from '../../images/logos/gco-2.png'
+import gco_3 from '../../images/logos/gco-3.png'
+import gco_4 from '../../images/logo.png'
+
+import nzc_1 from '../../images/logos/nzc-1.svg'
+import nzc_2 from '../../images/logos/nzc-2.png'
+import nzc_3 from '../../images/logos/nzc-3.png'
+import nzc_4 from '../../images/logos/nzc-4.png'
+import nzc_5 from '../../images/logos/nzc-5.svg'
+import nzc_6 from '../../images/logos/nzc-6.jpg'
+import nzc_7 from '../../images/logos/nzc-7.png'
 
 const Organizers = () => {
 
@@ -120,6 +143,307 @@ const Organizers = () => {
         }
     ]
 
+    const challenge_set_people = [
+        {
+            category: 'Corporate Social Responsibility',
+            people_details: [
+                {
+                    sponsor: csr_1,
+                    width: '130px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: csr_2,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                        {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        },
+                        {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: csr_3,
+                    width: '130px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            category: 'Cities and Municipalities',
+            people_details: [
+                {
+                    sponsor: cm_1,
+                    width: '130px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: cm_2,
+                    width: '130px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: cm_3,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: cm_4,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            category: 'Global Causes and Organizations',
+            people_details: [
+                {
+                    sponsor: 'PRME',
+                    width: '100px',
+                    is_string: true,
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: gco_1,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: gco_2,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: gco_4,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: gco_3,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                }
+            ]
+        },
+         {
+            category: 'Net-Zero Goals',
+            people_details: [
+                {
+                    sponsor: nzc_2,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: nzc_3,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: nzc_4,
+                    width: '100px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: nzc_6,
+                    width: '60px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                },
+                {
+                    sponsor: nzc_7,
+                    width: '60px',
+                    people: [
+                        {
+                            name: 'Shubham Sharma',
+                            title: 'Student Success Officer',
+                            email: 'ssharma470@myseneca.ca'
+                        },
+                         {
+                            name: 'Nishant Adekar',
+                            title: 'Student Success Officer',
+                            email: 'nadekar@myseneca.ca'
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+
     return (
         <section className="hero is-medium is-white">
             <div className="hero-body hero-body-2">
@@ -127,11 +451,11 @@ const Organizers = () => {
                     <div className='column is-10 is-offset-1'>
                     <br/>
                     <br/>
-                    <h1 className="title">Our Organizing Committee</h1><br/>
+                    <h1 className="title">Meet Our Organizers</h1><br/><br/>
                         <div className='organizers columns flex-wrap'>
                             {
                                 organizers.map((organizer, index) => (
-                                    <div key={index} className='column is-4-tablet is-3-desktop'>
+                                    <div key={index} className='column is-4-tablet is-2-5-desktop'>
                                         <div className="card card-3">
                                             <div className="card-image is-rounded-container">
                                                 <figure className="image is-1by1">
@@ -154,6 +478,35 @@ const Organizers = () => {
                             }
                         </div>
                     </div>
+                </div>
+                <div className='column is-10 is-offset-1'>
+                    <h1 className="title">Challenge Set Organizers</h1>
+                    {
+                        challenge_set_people.map(challenge => (
+                            <div className="challenge-container">
+                                <h1 className="subtitle">{challenge.category}</h1><br/>
+                                <div className="columns flex-wrap">
+                                    {
+                                        challenge.people_details.map(people_data => (
+                                            <div className="sponsor-item column is-12-mobile is-6-tablet is-4-desktop is-flex is-align-items-center">
+                                                <div className="sponsor-image">{people_data.is_string ? people_data.sponsor : <img src={people_data.sponsor} alt="sponsor" width={people_data.width}/>}</div>
+                                                <div className="is-flex is-flex-direction-column people-list">
+                                                {
+                                                    people_data.people.map(people => (
+                                                        <div className="people-item">
+                                                            <p className="name">{people.name}</p>
+                                                            <p className="desig">{people.title} {people.email ? <a target='_blank' rel="noopener noreferrer" href={`mailto:${people.email}`} className='email-icon'><FontAwesomeIcon icon={faEnvelope}/></a> : null}</p>
+                                                        </div>
+                                                    ))
+                                                }
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        ))  
+                    }
                 </div>
             </div>
         </section>
